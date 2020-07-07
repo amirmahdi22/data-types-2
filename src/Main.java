b763bfb21361f72fc4fcd2e5d37f25a0a5daa329
unknown <peyroviamirmahdi@gmail.com>
2020-07-07 09:47:50 +0430

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        int myint = 5 / 2;
+        float myfloat = 5 / 2f;
+        double mydouble = 5 / 2d;
+        System.out.println("myint :" + myint);
+        System.out.println("myfloat :" + myfloat);
+        System.out.println("mydouble :" + mydouble);
+        //--------------------------------------------------
+        double cm = 3;
+        double m = cm * 100;
+        System.out.println("result :" + m);
+        //-----------------------------------------
+
+    }
+}
